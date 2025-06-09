4f089cd0f3f8c9233cae89090a075a95db5ce727
Trinh Huu Tuan <79324181+tuanhuu3264@users.noreply.github.com>
2025-06-09 21:43:56 +0700

    Update app.js

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ import userRouter from "./routers/user-router.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
